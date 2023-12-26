b6ca6773fb15bd11bb0edf1fecb7668e799f56ee
asita26 <asita98890@gmail.com>
2023-12-26 10:39:34 +0530

    Adding test cases for step 3

--- test/java/org/example/InvoiceGeneratorTest.java
+++ test/java/org/example/InvoiceGeneratorTest.java
@@ -18,20 +18,29 @@ class InvoiceGeneratorTest {
     void testCalculateFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
 
-      // Test case 1: Single ride
+        // Test case 1: Single ride
         List<Ride> rides1 = new ArrayList<>();
         rides1.add(new Ride(2.5, 5));
-        assertEquals(30.0, invoiceGenerator.calculateTotalFare(rides1));
+        Invoice invoice1 = invoiceGenerator.calculateInvoice(rides1);
+        assertEquals(1, invoice1.getTotalRides());
+        assertEquals(30.0, invoice1.getTotalFare());
+        assertEquals(30.0, invoice1.getAverageFare());
 
-     // Test case 2: Multiple rides
+        // Test case 2: Multiple rides
         List<Ride> rides2 = new ArrayList<>();
         rides2.add(new Ride(2.5, 5));
         rides2.add(new Ride(10.0, 60));
         rides2.add(new Ride(0.1, 1));
-        assertEquals(195.0, invoiceGenerator.calculateTotalFare(rides2));
+        Invoice invoice2 = invoiceGenerator.calculateInvoice(rides2);
+        assertEquals(3, invoice2.getTotalRides());
+        assertEquals(195.0, invoice2.getTotalFare());
+        assertEquals(65.0, invoice2.getAverageFare(), 0.01);
 
         // Test case 3: No rides
         List<Ride> rides3 = new ArrayList<>();
-        assertEquals(0.0, invoiceGenerator.calculateTotalFare(rides3));
+        Invoice invoice3 = invoiceGenerator.calculateInvoice(rides3);
+        assertEquals(0, invoice3.getTotalRides());
+        assertEquals(0.0, invoice3.getTotalFare());
+        assertEquals(0.0, invoice3.getAverageFare());
     }
 }
